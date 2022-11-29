beb0dfff6cfaa5dae8d335365ad2fc77b5de534e
Yukinevv <adrianrodzic33@gmail.com>
2022-11-29 10:44:38 +0100

    zad4-edycja

--- zad4/zad4.js
+++ zad4/zad4.js
@@ -1,14 +1,15 @@
 const sum = arr => {
 
-    let num = 0;
+    //let num = 0;
     /*arr.forEach(element => {
         num += element;
     });
     return num;*/
 
-    return arr.reduce((acc, currVal) => acc += currVal, num);
+    return arr.reduce((acc, currVal) => acc += currVal, 0);
 };
 
-document.querySelector('#wynik').innerHTML = sum([3, 5, 7]) + "<br/>";
+console.log(sum([3, 5, 7]));
+/*document.querySelector('#wynik').innerHTML = sum([3, 5, 7]) + "<br/>";
 document.querySelector('#wynik').innerHTML += sum([12, 5, 7]) + "<br/>";
-document.querySelector('#wynik').innerHTML += sum([25, 5, 7]);
+document.querySelector('#wynik').innerHTML += sum([25, 5, 7]);*/
