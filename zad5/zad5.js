46ee277f106ae4bc3788159cbec25b05bfab418f
Yukinevv <adrianrodzic33@gmail.com>
2022-11-29 10:51:39 +0100

    zad5

---
+++ zad5/zad5.js
@@ -0,0 +1,10 @@
+const fibonacci = n => {
+    let arr = [0, 1];
+
+    for (i = 2; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr;
+};
+
+console.log(fibonacci(9));
